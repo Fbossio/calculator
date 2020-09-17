01c9df93b448e9dcd07f4a3ea62e0db961d37065
Felix Bossio <felixbossio@gmail.com>
2020-09-17 18:45:50 -0500

    Updates

---
+++ precache-manifest.497192261fddfc55a7f1f326ad6111de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcf445dc4a6473c0fd3e9dbebea2aad",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "80a09631ffec14608bd8",
+    "url": "/calculator/static/css/main.3a6f97b2.chunk.css"
+  },
+  {
+    "revision": "34847a56b309d193000a",
+    "url": "/calculator/static/js/2.1c56c3e1.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.1c56c3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a09631ffec14608bd8",
+    "url": "/calculator/static/js/main.eacb98f8.chunk.js"
+  },
+  {
+    "revision": "ae29c0152cb0c28bb4291c60ca270c0b",
+    "url": "/calculator/static/js/main.eacb98f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
